f6b13bd48a91e7362c29a1a7900f439562c0c979
alexa <alexa.lashkevich@gmail.com>
2024-06-22 20:46:32 +0300

    srthrtsh

--- helper.py
+++ helper.py
@@ -1,78 +1,116 @@
-﻿from speech_recognition.recognizers import google
+﻿#импортирование необходимых библиотек и данных из других файлов
+from speech_recognition.recognizers import google
 import speech_recognition as sr
 from gtts import gTTS
 import os
 from contextlib import suppress
 from typing import Callable, Optional
-from consts import names, commands, mode_regex, modes, from_language, to_language, from_language_default, to_language_default, languages
+from consts import names, commands, mode_regex, modes, from_language
+from consts import to_language, from_language_default, to_language_default, languages
 import pymorphy3
 import re
 from deep_translator import GoogleTranslator
 
 
-
+# инструмент для анализа слова
 morph = pymorphy3.MorphAnalyzer()
 
+
+# запись речи, из которой будут определяться упоминания, команды и тп
 def record_sample(r: sr.Recognizer, language) -> str:
+    
+    # для записи используется микрофон
     with sr.Microphone() as mic:
         audio = r.listen(mic)
 
-        '''
-    lang:str
-    
-    if mode == 'normal':
-        lang = 'ru'
-    else:
-        lang = from_language
-        '''
-        
-
+    # функция переводит аудио в строку и возвращает его в виде текста
     return google.recognize_legacy(r, audio, language=str(language)).lower()
 
 
+# удаление упоминания робота из текста
 def remove_mention(text: str) -> tuple[str, bool]:
+    
+    # переменная flag показывает наличие или отсутствие упоминания
+    # по дефлоту она принимает значение, равное логическому нулю
     flag = False
+
+    # перебираются все возможные имена
     for name in names:
+
+        # если имя встречается в тексте, оно заменяется пустой строкой
+        # и переменная flag принимает значение, равное логической единице
         if name in text:
             text = text.replace(name, " ")
             flag = True
+
+        # функция возвращает текст без упоминания и информацию о наличии или отсутствии упоминания
     return text, flag
 
 
+# функция для определения комманды (регулярные выражения)
 def detect_command(text: str) -> Optional[tuple[Callable, tuple[str]]]:
+
+    # перебирается список регулярных выражений на совпадения принятого текста с элементом сиска
     for regex in commands:
         obj = re.findall(regex, text)
+
+    # если команда возвращается - она возвращается и вызывает соответствующую функцию
         if obj:
             return commands[regex], tuple(obj)
-
+        
     return None
 
 
+# фунуция для синтеза речи
 def say_text(text: str, lang: str = 'ru'):
+
+    # текст, который будет произноситься, до этого записывается в память или выводится
+    # после этого текст переводится в аудиоформат
     filename = "output.mp3"
+
+    # если файл существует, он удаляется из памяти
     with suppress(FileNotFoundError):
         os.remove(filename)
+
+    # при помощи библиотеки gTTS текст преобразуется в речь
     res = gTTS(text=text, lang=lang)
+
+    # результат сохраняется в аудиоформате
     res.save(filename)
 
+
+    # файл проигрывается в системе (команда автоматически вводится в терминал компьютера)
+
+    # для плеера windows
     if os.name == 'nt':  # Windows
         os.system('start ' + filename)
+
+    # для плеера mac
     else:  # Unix
         os.system('afplay ' + filename)
 
-def translate(text: str, from_lang: str, to_lang: str) -> str:
-    return GoogleTranslator(source=from_lang, target=to_lang).translate(text)
-    # return argostranslate.translate.translate(text, from_lang, to_lang)
-   
 
+# функция для перевода
+def translate(text: str, from_lang: str, to_lang: str) -> str:
+
+    # возвращает исходный текст, переведенный на запрошенный язык
+    return GoogleTranslator(source=from_lang, target=to_lang).translate(text)
+    
+   
+# функция для возвращения слова в начальную форму
+# преобразование слов осуществляется при помощи модуля зньўкзр3
 def get_normal_form(text: str) -> str:
+
+    # возврвщает исходное слово в начальную форму
     return morph.parse(text)[0].normal_form
 
 
+# функция для смены режима 
 def change_mode(current_mode: str, text: str) -> tuple[str, str]:
+
     if len(k := re.findall(mode_regex, text)) == 0:
         return current_mode, ""
-
+    
     a, b = k[0]
     c = a + b 
     c, *args = c.split()
@@ -81,9 +119,11 @@ def change_mode(current_mode: str, text: str) -> tuple[str, str]:
 
     current_mode = modes.get(c, current_mode)
 
+    # функция возвращает режим и его параметры
     return current_mode, args
 
 
+# функция для определения языков, с которого и на который будет осуществляться перевод
 def get_language(text: str) -> tuple[str, str]:
     k = re.findall(from_language, text)
     from_text = from_language_default if len(k) == 0 else k[0]
@@ -96,3 +136,4 @@ def get_language(text: str) -> tuple[str, str]:
     to_text = languages.get(to_text, to_language_default)
 
     return from_text, to_text
+
--- modes.py
+++ modes.py
@@ -1,36 +1,69 @@
-from helper import detect_command, remove_mention, say_text, translate as helper_translate, get_language
+# импортирование необходимых данных из других файлов 
+from helper import detect_command, remove_mention, say_text, translate as helper_translate, get_language, change_mode
 import functions
 
+# импортирование модуля для перевода
+from deep_translator import GoogleTranslator
 
+
+# нормальный режим
 def normal(text: str, _) -> None:
+
+    # в нормальном режиме робот принимает команды только на русском языке
     language = 'ru'
+
+    # определение и удаление имени робота из принятого текста
     text, detect = remove_mention(text)
+
+    # если имя не упоминалось,команда не будет принята и выведется текст об отсутствии упоминания
     if not detect:
         print("No mention")
         return
+    
 
+    # определение команды
     command_info = detect_command(text)
+
+    # если не удалось определить никакую команду, информация об этом выводится
     if command_info is None:
         print("No command")
         return
+    
+    # команда и её параметры возвращаются функцией  detect_command
     command, args = command_info
 
+
+    # вывод распознанной команды
     print(f"Command named {command} found")
+
+    # определение того, что надо вывести или сказать
+    # формируется из того, что возвращает функция, вызванная командой
     output = getattr(functions, command)(*args)
     if output is None:
         print("No output")
         return
+    
+    # синтез речи из аутпута
     say_text(output)
 
 
+# режим превода
 def translate(text: str, data: str) -> None:
+
+    # получение языков перевода, которые возвращает функция get_language()
     fl, tl = get_language(data)
-    if text == 'роберт хватит':
+
+    #если на языке, с которого осуществляется перевод, сказать "стоп", программа завершит работу                                              
+    if text == GoogleTranslator(source='ru', target =fl).translate('стоп'):
         exit()
+
+    elif GoogleTranslator(source='ru', target=fl).translate('перейди в режим перевода') in text:
+        change_mode(normal, translate, data)
     
-    elif 'роберт перейди в режим перевода' in text:
-        translate()
+    elif text == GoogleTranslator(source='ru', target=fl).translate('вернись в нормальный режим'):
+        pass
 
     else:
            trans = helper_translate(text, fl, tl)
            say_text(trans, tl)
+
